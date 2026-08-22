d3e135035154832986165a895ba6dbd4be082e19
Mara <mara.crespo.tech@gmail.com>
2026-08-22 23:09:25 +0200

    feat: add postsByLang filter and posts collection

--- blog/.eleventy.js
+++ blog/.eleventy.js
@@ -1,7 +1,13 @@
 const readingTime = require("./eleventy/filters/reading-time.js");
+const postsByLang = require("./eleventy/filters/posts-by-lang.js");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("readingTime", readingTime);
+  eleventyConfig.addFilter("postsByLang", postsByLang);
+
+  eleventyConfig.addCollection("posts", (collectionApi) =>
+    collectionApi.getFilteredByGlob("posts/*.md").sort((a, b) => b.date - a.date)
+  );
 
   return {
     dir: {
